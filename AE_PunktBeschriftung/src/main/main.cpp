5bad7b5c2e5a87520a5c17ee6c1c76abd10ae608
mhuendorf <163-mhuendorf@users.noreply.vm625.rz.uni-osnabrueck.de>
2020-11-10 16:50:27 +0100

    fixed clang warnings

--- AE_PunktBeschriftung/src/main/main.cpp
+++ AE_PunktBeschriftung/src/main/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+
 using std::cout;
 using std::endl;
 
 #include <string>
+
 using std::string;
 
 #include <fstream>
@@ -12,28 +14,28 @@ using std::string;
 #include <io/InstanceReader.hpp>
 #include <solver/TrivialSolver.hpp>
 
-bool fexists(const string& filename) {
-  std::ifstream ifile(filename.c_str());
-  return (bool) ifile;
+bool fexists(const string &filename) {
+    std::ifstream ifile(filename.c_str());
+    return (bool) ifile;
 }
 
 // didn't know this, but abort() is an internal name
 void abortProgram() {
-    cout << "Could not parse command line arguments!\n" 
-        << "Make sure that all cmd arguments were spelled correctly and that files exist." << endl;
+    cout << "Could not parse command line arguments!\n"
+         << "Make sure that all cmd arguments were spelled correctly and that files exist." << endl;
 }
 
-void checkFeasibility(const string& filename) {
+void checkFeasibility(const string &filename) {
 
     Instance instance = readInstance(filename);
 
-    if(instance.isFeasible()) {
+    if (instance.isFeasible()) {
         cout << instance.countLabelledPoints() << endl;
-    } 
+    }
 
 }
 
-void solve(const string& infile, const string& outfile) {
+void solve(const string &infile, const string &outfile) {
 
     Instance instance = readInstance(infile);
 
@@ -52,23 +54,23 @@ void solve(const string& infile, const string& outfile) {
 
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
     try {
-    
-        if(argc == 3) {
+
+        if (argc == 3) {
             string eval = argv[1];
-            if(eval.compare("-eval") == 0 && fexists(argv[2])) {
+            if (eval == "-eval" && fexists(argv[2])) {
                 checkFeasibility(argv[2]);
             } else {
                 abortProgram();
                 return 0;
             }
-        } else if(argc == 5) {
+        } else if (argc == 5) {
             string in = argv[1];
             string out = argv[3];
-            if( in.compare("-in") == 0 && out.compare("-out") == 0 && fexists(argv[2]) ) {
-                
+            if (in == "-in" && out == "-out" && fexists(argv[2])) {
+
                 solve(argv[2], argv[4]);
 
             } else {
@@ -78,13 +80,13 @@ int main(int argc, char* argv[]) {
 
         } else {
             cout << "This program can be started with either one or two command line arguments.\n"
-                << "Call './solver -in dat1 -out dat2' to read in dat1, run the algorithm and write the solution to dat2.\n"
-                << "Call './solver -eval dat1' to read in dat1 and check a solution for feasibility."
-                << endl;
+                 << "Call './solver -in dat1 -out dat2' to read in dat1, run the algorithm and write the solution to dat2.\n"
+                 << "Call './solver -eval dat1' to read in dat1 and check a solution for feasibility."
+                 << endl;
         }
         return 0;
-    
-    } catch(const std::exception& e) {
+
+    } catch (const std::exception &e) {
         cout << "ERROR: " << e.what() << endl;
     }
 }
